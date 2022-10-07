df59cc87bd78f3e118034aa33616c9907aa11a95
pina7-cyber <patricia.borovski@web.de>
2022-10-07 16:36:36 +0200

    build file

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('build'))
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
